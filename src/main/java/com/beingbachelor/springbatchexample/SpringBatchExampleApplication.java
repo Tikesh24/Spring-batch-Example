16cae58b046ec602c0a8a29742e5ff8004301de2
Verma <tver3009@7-11.com>
2022-07-20 13:53:58 +0530

    Added reference document

--- src/main/java/com/beingbachelor/springbatchexample/SpringBatchExampleApplication.java
+++ src/main/java/com/beingbachelor/springbatchexample/SpringBatchExampleApplication.java
@@ -1,8 +1,5 @@
 package com.beingbachelor.springbatchexample;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/beingbachelor/springbatchexample/controller/LoadController.java
+++ src/main/java/com/beingbachelor/springbatchexample/controller/LoadController.java
@@ -1,9 +1,8 @@
 package com.beingbachelor.springbatchexample.controller;
 
-import com.beingbachelor.springbatchexample.model.User;
 import com.beingbachelor.springbatchexample.service.JobLauncherService;
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -11,9 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/load")
